efeb3bc3b10d44b1a337c712a1dac95deee17ea2
solakmirnes <solakmirnes@gmail.com>
2022-04-06 14:48:16 +0200

    added vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,12 @@ require('./bootstrap');
 
 import Alpine from 'alpinejs';
 
+import { createApp } from 'vue'
+
+import App from './components/App.vue'
+
+createApp(App).mount('#app')
+
 window.Alpine = Alpine;
 
 Alpine.start();
---
+++ resources/js/components/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <div class="max-w-7xl">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style scoped>
+
+
+</style>
